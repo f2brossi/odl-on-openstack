98b0b5b0499408167ea725b7967dbb5891af7ecd
rossi <f2brossi@gmail.com>
2015-06-13 23:41:02 +0200

    add puppet directory for compute

---
+++ compute/puppet/manifests/devstack-compute.pp
@@ -0,0 +1,18 @@
+vcsrepo {'/home/stack/devstack':
+    ensure   => present,
+    provider => git,
+    user     => 'stack',
+    source   => 'https://github.com/openstack-dev/devstack.git',
+    # source   => 'https://github.com/flavio-fernandes/devstack.git',
+    # revision => 'odlDevel',
+    before   => File['/home/stack/devstack/local.conf'],
+}
+
+$hosts = hiera('hosts')
+
+file { '/home/stack/devstack/local.conf':
+    ensure  => present,
+    owner   => 'stack',
+    group   => 'stack',
+    content => template('/vagrant/puppet/templates/compute.local.conf.erb'),
+}
